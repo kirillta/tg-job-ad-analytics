7f26e33e0ab9977f12a85ebc5795b2bec77ca9f1
Kirill Taran <kirill.taran@hotmail.com>
2025-06-29 14:49:57 -0300

    Add migration for AdEntity and update model snapshot.

---
+++ src/TgJobAdAnalytics/Migrations/20250629174233_AddAdEntity.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TgJobAdAnalytics.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAdEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ads",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Date = table.Column<DateOnly>(type: "TEXT", nullable: false),
+                    Text = table.Column<string>(type: "TEXT", nullable: false),
+                    MessageId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ads", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ads_MessageId",
+                table: "Ads",
+                column: "MessageId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ads");
+        }
+    }
+}
